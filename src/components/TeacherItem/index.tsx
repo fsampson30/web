b9f2ff942d0ef325bf2705efa1ef7e777494d3b5
fsampson30 <fsampson30@gmail.com>
2022-08-30 11:05:23 -0300

    Fetched teachers.

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,30 +1,55 @@
 import React from "react";
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.png'
+import api from "../../services/api";
 
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher {
+    avatar: string
+    bio: string
+    cost: number
+    id: number
+    name: string
+    subject: string
+    user_id: string
+    whatsapp: string
+
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections',{
+            user_id: teacher.id
+        })
+
+    }
+
     return (
-    <article className="teacher-item">
-    <header>
-        <img src="https://avatars.githubusercontent.com/u/71216861?v=4" alt="Flavio Sampson" />
-        <div>
-            <strong>Flavio Sampson</strong>
-            <span>Química</span>
-        </div>
-    </header>
-    <p>Exemplo de texto</p>
-    <footer>
-        <p>Preço/hora
-            <strong>R$ 80,00</strong>
-        </p>
-        <button type="button">
-            <img src={whatsappIcon} alt="Whatsapp" />
-            Entrar em contato
-        </button>
-    </footer>
-</article>
+        <article className="teacher-item">
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+            <p>{teacher.bio}</p>
+            <footer>
+                <p>Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a onClick={createNewConnection} href={`http://wa.me/${teacher.whatsapp}`}>
+                    <img src={whatsappIcon} alt="Whatsapp" />
+                    Entrar em contato
+                </a>
+            </footer>
+        </article>
     );
 }
 
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,48 +1,75 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader/index";
 import Select from "../../components/Select";
-import TeacherItem from "../../components/TeacherItem";
+import TeacherItem, {Teacher} from "../../components/TeacherItem";
+import api from "../../services/api";
 
 import './styles.css';
 
 
-function TeacherList(){
-    return(
+
+function TeacherList() {
+
+    const [teachers, setTeachers] = useState([])
+
+    const [subject, setSubject] = useState('')
+    const [week_day, setWeekday] = useState('')
+    const [time, setTime] = useState('')
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault()
+
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        })
+
+        setTeachers(response.data)
+
+    }
+
+
+    return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os Proffys disponíveis.">
-                <form id="search-teachers">
-                <Select name="subject" label="Matéria" 
-                    options={[
-                        {value: "Artes", label: "Artes"},
-                        {value: "Biologia", label: "Biologia"},
-                        {value: "Matematica", label: "Matemática"},
-                        {value: "Fisica", label: "Física"}
-                    ]
-                    } />
+                <form id="search-teachers" onSubmit={searchTeachers}>
+                    <Select name="subject" label="Matéria"
+                        value={subject}
+                        onChange={e => { setSubject(e.target.value) }}
+                        options={[
+                            { value: "Artes", label: "Artes" },
+                            { value: "Biologia", label: "Biologia" },
+                            { value: "Matematica", label: "Matemática" },
+                            { value: "Fisica", label: "Física" }
+                        ]
+                        } />
 
-                <Select name="week_day" label="Dia da Semana" 
-                    options={[
-                        {value: "0", label: "Domingo"},
-                        {value: "1", label: "Segunda-feira"},
-                        {value: "2", label: "Terça-feira"},
-                        {value: "3", label: "Quarta-feira"},
-                        {value: "4", label: "Quinta-feira"},
-                        {value: "5", label: "Sexta-feira"},
-                        {value: "6", label: "Sábado"},
-                    ]
-                    } />
-                                       
-                    <Input type="time" name="time" label="Hora" />      
+                    <Select name="week_day" label="Dia da Semana"
+                        value={week_day}
+                        onChange={e => { setWeekday(e.target.value) }}
+                        options={[
+                            { value: "0", label: "Domingo" },
+                            { value: "1", label: "Segunda-feira" },
+                            { value: "2", label: "Terça-feira" },
+                            { value: "3", label: "Quarta-feira" },
+                            { value: "4", label: "Quinta-feira" },
+                            { value: "5", label: "Sexta-feira" },
+                            { value: "6", label: "Sábado" },
+                        ]
+                        } />
+
+                    <Input type="time" name="time" label="Hora" value={time} onChange={e => { setTime(e.target.value) }} />
+                    <button type="submit">Buscar</button>
                 </form>
             </PageHeader>
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
             </main>
 
         </div>
